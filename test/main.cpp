bab9521de1286fad535daa01f9c7a86b8a018679
Kaj-Michael Lang <milang@tal.org>
2016-11-23 16:15:55 +0200

    Add start of "find decoder by file extension" feature

--- test/main.cpp
+++ test/main.cpp
@@ -2,46 +2,120 @@
 #include <QObject>
 #include <QDirIterator>
 #include <QFile>
+#include <QFileInfo>
 #include <QStringListModel>
 #include <QIODevice>
+#include <QHash>
+#include <QPair>
+#include <QTime>
 #include <QDebug>
 #include "audiooutput.h"
 
 QStringListModel files;
+QList<QPair<QStringList, CMBaseAudioSource *> * >decoders;
+QHash<QRegExp *, CMBaseAudioSource *> regToDecoder;
 
-void scan_dir(const QString &path)
+bool scanDirectories(QStringList &paths, const QStringList &filter, QStringList &list)
 {
-    QStringList bob;
-    bob << "*.sid";
+    if (paths.isEmpty())
+        return false;
 
-    QDirIterator it(path, bob);
+    const QString path=paths.takeFirst();
+    QDirIterator it(path, filter, QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
+
     while (it.hasNext()) {
-        QFile file(it.next());
-        if (file.open(QIODevice::ReadOnly)) {
-            qDebug() << "Opened:" << file.fileName() << endl;
-        }else {
-            qDebug() << "Can't open " << file.fileName() << file.errorString() << endl;
+        QString f;
+        QFileInfo info;
+
+        f=it.next();
+
+        info=it.fileInfo();
+        if (info.isFile())
+            list.append(f);
+        else if (info.isDir()) {
+            paths.append(f);
         }
-        if(it.hasNext()==false){
-            bob.clear();
-            qDebug() << "Finished";
+    }
+
+    return !paths.isEmpty();
+}
+
+void createDecoderRegExp()
+{
+    for (int i = 0; i < decoders.size(); i++) {
+        QStringList filters=decoders.at(i)->first;
+        for (int f = 0; f < filters.size(); f++) {
+            QRegExp *r=new QRegExp(filters.at(f), Qt::CaseInsensitive, QRegExp::Wildcard);
+            regToDecoder.insert(r, decoders.at(i)->second);
         }
     }
 }
 
+CMBaseAudioSource *findSuitableDecoder(QString file)
+{
+    QHashIterator<QRegExp *, CMBaseAudioSource *> i(regToDecoder);
+    while (i.hasNext()) {
+        i.next();
+        QRegExp *r=i.key();
+        if (r->exactMatch(file))
+            return i.value();
+    }
+
+    qWarning() << "Failed to find suitable decoder for file: " << file;
+
+    return NULL;
+}
+
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
     int r;
+    bool mr;
+    QStringList filter;
+    QStringList fileList;
+    QStringList directoryStack;
+    AudioTest *at;
+    CMBaseAudioSource *decoder;
 
     app.setApplicationName("Qt CM Player test");
 
-    scan_dir("/home/milang/Music/sidmusic");
+    qsrand(QTime::currentTime().msec());
 
-    AudioTest *at;
+    filter << CMModPlugAudioSource::extensions();
+    filter << CMSidAudioSource::extensions();
+    filter << CMSapAudioSource::extensions();
+
+    directoryStack << "/home/milang/Music";
+
+    while (scanDirectories(directoryStack, filter, fileList)) {
+        // XXX
+    }
+
     at=new AudioTest();
 
-    r=app.exec();
+    decoders.append(new QPair<QStringList, CMBaseAudioSource *>(CMModPlugAudioSource::extensions(), static_cast<CMBaseAudioSource*>(new CMModPlugAudioSource(at))));
+
+    decoders.append(new QPair<QStringList, CMBaseAudioSource *>(CMSidAudioSource::extensions(), static_cast<CMBaseAudioSource*>(new CMSidAudioSource(at))));
+
+    decoders.append(new QPair<QStringList, CMBaseAudioSource *>(CMSapAudioSource::extensions(), static_cast<CMBaseAudioSource*>(new CMSapAudioSource(at))));
+
+    qDebug() << decoders;
+
+    createDecoderRegExp();
+
+    QString rfile=fileList.at(qrand() % fileList.size());
+
+    qDebug() << "Playing file: " << rfile;
+
+    decoder=findSuitableDecoder(rfile);
+    mr=at->load(rfile, decoder);
+
+    if (mr) {
+        at->play();
+        r=app.exec();
+    } else {
+        r=255;
+    }
 
     delete at;
 

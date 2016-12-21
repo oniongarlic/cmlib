6408367abbe52e9a5d0454a604a27758b53aac79
Kaj-Michael Lang <milang@tal.org>
2016-12-21 17:17:57 +0200

    Add async scanning

--- sources/cmmediascanner.cpp
+++ sources/cmmediascanner.cpp
@@ -4,7 +4,9 @@
 
 CMMediaScanner::CMMediaScanner(QObject *parent) : QObject(parent)
 {
-
+    m_ticker.setSingleShot(true);
+    m_ticker.setInterval(100);
+    connect(&m_ticker, SIGNAL(timeout()), this, SLOT(scanLoop()));
 }
 
 void CMMediaScanner::setFilters(const QStringList &filter)
@@ -35,6 +37,23 @@ void CMMediaScanner::clearPaths()
     m_paths.clear();
 }
 
+bool CMMediaScanner::scanAsync()
+{
+    if (m_paths.isEmpty())
+        return false;
+
+    if (scan(m_filelist, true))
+        m_ticker.start();
+
+    return true;
+}
+
+void CMMediaScanner::scanLoop()
+{
+    if (scan(m_filelist, false))
+        m_ticker.start();
+}
+
 /**
  * @brief CMMediaScanner::scanDirectories
  * @param paths
@@ -42,14 +61,14 @@ void CMMediaScanner::clearPaths()
  * @param list
  * @return
  */
-bool CMMediaScanner::scan(QStringList &list)
+bool CMMediaScanner::scan(QStringList &list, bool fromStart)
 {
     bool r;
 
     if (m_paths.isEmpty())
         return false;
 
-    if (m_pathsleft.isEmpty())
+    if (m_pathsleft.isEmpty() || fromStart)
         m_pathsleft=m_paths;
 
     const QString path=m_pathsleft.takeFirst();
@@ -77,3 +96,5 @@ bool CMMediaScanner::scan(QStringList &list)
 
     return r;
 }
+
+
--- sources/cmmediascanner.h
+++ sources/cmmediascanner.h
@@ -7,6 +7,7 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QStringList>
+#include <QTimer>
 
 class CMMediaScanner : public QObject
 {
@@ -16,7 +17,8 @@ public:
     Q_INVOKABLE void setFilters(const QStringList &filter);
     Q_INVOKABLE bool addPath(const QString path);
     Q_INVOKABLE void clearPaths();
-    Q_INVOKABLE bool scan(QStringList &list);
+    Q_INVOKABLE bool scan(QStringList &list, bool fromStart=false);
+    Q_INVOKABLE bool scanAsync();
 
 signals:
     void scanning(QString path);
@@ -24,10 +26,15 @@ signals:
 
 public slots:
 
+private slots:
+    void scanLoop();
+
 private:
     QStringList m_pathsleft;
     QStringList m_paths;
     QStringList m_filter;
+    QTimer m_ticker;
+    QStringList m_filelist;
 };
 
 #endif // CMMEDIASCANNER_H

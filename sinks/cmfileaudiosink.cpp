66f4fa2689d1f436d752ddf54d85df186945df30
Kaj-Michael Lang <milang@tal.org>
2017-01-02 15:43:17 +0200

    Add start of a raw data file writer sink

---
+++ sinks/cmfileaudiosink.cpp
@@ -0,0 +1,86 @@
+#include "cmfileaudiosink.h"
+
+CMFileAudioSink::CMFileAudioSink(QObject *parent) :
+    CMBaseAudioSink(parent),
+    m_readSize(44100*2*2)
+{
+    m_timer.setInterval(100);
+    connect(&m_timer, SIGNAL(timeout()), SLOT(readTicker()));
+}
+
+bool CMFileAudioSink::play()
+{
+    bool r;
+
+    if (!m_source) {
+        qWarning("Source not set!");
+        return false;
+    }
+
+    if (m_file.fileName().isEmpty()) {
+        qWarning("File name not set");
+        return false;
+    }
+
+    if (!m_source->isOpen()) {
+        qDebug("Opening source");
+        r=m_source->open(QIODevice::ReadOnly);
+        if (!r) {
+            qWarning("Open for playback failed");
+            return false;
+        }
+    }
+
+    if (m_file.isOpen()) {
+        m_state=QAudio::ActiveState;
+        m_timer.start();
+        return true;
+    }
+
+    r=m_file.open(QIODevice::WriteOnly);
+
+    if (r) {
+        m_timer.start();
+    } else {
+        qWarning("Failed to open file for writing");
+    }
+
+    return r;
+}
+
+bool CMFileAudioSink::stop()
+{
+    if (!m_file.isOpen())
+        return false;
+
+    aboutToClose();
+
+    m_file.close();
+    //m_file.disconnect(this, 0);
+
+    m_state=QAudio::StoppedState;
+
+    return true;
+}
+
+bool CMFileAudioSink::pause()
+{
+    m_timer.stop();
+    m_state=QAudio::IdleState;
+}
+
+bool CMFileAudioSink::setFile(const QString file)
+{
+    m_file.setFileName(file);
+}
+
+void CMFileAudioSink::readTicker()
+{
+    QByteArray tmp=m_source->read(m_readSize);
+    m_file.write(tmp);
+}
+
+bool CMFileAudioSink::aboutToClose()
+{
+    return true;
+}
---
+++ sinks/cmfileaudiosink.h
@@ -0,0 +1,34 @@
+#ifndef CMFILEAUDIOSINK_H
+#define CMFILEAUDIOSINK_H
+
+#include "cmbaseaudiosink.h"
+
+#include <QFile>
+#include <QTimer>
+
+class CMFileAudioSink : public CMBaseAudioSink
+{
+    Q_OBJECT
+public:
+    CMFileAudioSink(QObject *parent = 0);
+
+    Q_INVOKABLE bool play();
+    Q_INVOKABLE bool stop();
+    Q_INVOKABLE bool pause();
+    //Q_INVOKABLE bool resume();
+
+    Q_INVOKABLE bool setFile(const QString file);
+
+protected slots:
+    void readTicker();
+
+protected:
+    virtual bool aboutToClose();
+
+private:
+    QTimer m_timer;
+    QFile m_file;
+    uint m_readSize;
+};
+
+#endif // CMFILEAUDIOSINK_H

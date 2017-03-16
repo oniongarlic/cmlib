c563c300a684a2526095977065649a71196be623
Kaj-Michael Lang <milang@tal.org>
2017-03-16 15:16:21 +0200

    Init sink to null

--- player/cmmediaplayer.cpp
+++ player/cmmediaplayer.cpp
@@ -14,6 +14,7 @@ CMMediaPlayer::CMMediaPlayer(QObject *parent)
     , m_length(0)
     , m_track(0)
     , m_tracks(0)
+    , m_sink(0)
 {
     connect(&m_dec, SIGNAL(metadata(QVariantHash)), this, SLOT(decoderMetadata(QVariantHash)));
     connect(&m_dec, SIGNAL(eot()), this, SLOT(decoderEOT()));

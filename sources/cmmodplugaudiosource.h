06aa0f7e45edba0695758f17a53fb1651b9355a9
Kaj-Michael Lang <milang@tal.org>
2018-01-08 13:52:15 +0200

    Add missing extensions

--- sources/cmmodplugaudiosource.h
+++ sources/cmmodplugaudiosource.h
@@ -10,7 +10,7 @@ class CMModPlugAudioSource : public CMBaseAudioSource
 {
     Q_OBJECT
     Q_CLASSINFO("mime", "audio/x-mod")
-    Q_CLASSINFO("extensions", "*.mod;*.xm")
+    Q_CLASSINFO("extensions", "*.mod;*.xm;*.it;*.s3m;*.stm;*.669;*.amf;*.ams;*.dbm;*.dmf;*.dsm;*.far;*.mdl;*.med;*.mtm;*.okt;*.ptm;*.ult;*.umx;*.mt2;*.psm")
 public:
     CMModPlugAudioSource(QObject *parent);
 
